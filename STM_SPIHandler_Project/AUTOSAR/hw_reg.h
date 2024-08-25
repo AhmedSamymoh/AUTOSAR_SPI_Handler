91561a6a74f59054840751397ef38459c5717f9b
RanaMohamed227 <rana.ali03@eng-st.cu.edu.eg>
2024-08-25 15:30:31 +0300

    Register mapping to spi2,3,4

--- STM_SPIHandler_Project/AUTOSAR/hw_reg.h
+++ STM_SPIHandler_Project/AUTOSAR/hw_reg.h
@@ -62,6 +62,49 @@
 #define SPI1_I2SCFGR           *((volatile uint32 *) (SPI1_BASE + SPI_I2SCFGR_OFFSET))
 #define SPI1_I2SPR             *((volatile uint32 *) (SPI1_BASE + SPI_I2SPR_OFFSET))
 
+
+/*********************************************/
+/*             SPI2 Registers                */
+/*********************************************/
+#define SPI2_CR1               *((volatile uint32 *) (SPI2_BASE + SPI_CR1_OFFSET))
+#define SPI2_CR2               *((volatile uint32 *) (SPI2_BASE + SPI_CR2_OFFSET))
+#define SPI2_SR                *((volatile uint32 *) (SPI2_BASE + SPI_SR_OFFSET))
+#define SPI2_DR                *((volatile uint32 *) (SPI2_BASE + SPI_DR_OFFSET))
+#define SPI2_CRCPR             *((volatile uint32 *) (SPI2_BASE + SPI_CRCPR_OFFSET))
+#define SPI2_RXCRCR            *((volatile uint32 *) (SPI2_BASE + SPI_RXCRCR_OFFSET))
+#define SPI2_TXCRCR            *((volatile uint32 *) (SPI2_BASE + SPI_TXCRCR_OFFSET))
+#define SPI2_I2SCFGR           *((volatile uint32 *) (SPI2_BASE + SPI_I2SCFGR_OFFSET))
+#define SPI2_I2SPR             *((volatile uint32 *) (SPI2_BASE + SPI_I2SPR_OFFSET))
+
+
+/*********************************************/
+/*             SPI3 Registers                */
+/*********************************************/
+#define SPI3_CR1               *((volatile uint32 *) (SPI3_BASE + SPI_CR1_OFFSET))
+#define SPI3_CR2               *((volatile uint32 *) (SPI3_BASE + SPI_CR2_OFFSET))
+#define SPI3_SR                *((volatile uint32 *) (SPI3_BASE + SPI_SR_OFFSET))
+#define SPI3_DR                *((volatile uint32 *) (SPI3_BASE + SPI_DR_OFFSET))
+#define SPI3_CRCPR             *((volatile uint32 *) (SPI3_BASE + SPI_CRCPR_OFFSET))
+#define SPI3_RXCRCR            *((volatile uint32 *) (SPI3_BASE + SPI_RXCRCR_OFFSET))
+#define SPI3_TXCRCR            *((volatile uint32 *) (SPI3_BASE + SPI_TXCRCR_OFFSET))
+#define SPI3_I2SCFGR           *((volatile uint32 *) (SPI3_BASE + SPI_I2SCFGR_OFFSET))
+#define SPI3_I2SPR             *((volatile uint32 *) (SPI3_BASE + SPI_I2SPR_OFFSET))
+
+
+/*********************************************/
+/*             SPI4 Registers                */
+/*********************************************/
+#define SPI4_CR1               *((volatile uint32 *) (SPI4_BASE + SPI_CR1_OFFSET))
+#define SPI4_CR2               *((volatile uint32 *) (SPI4_BASE + SPI_CR2_OFFSET))
+#define SPI4_SR                *((volatile uint32 *) (SPI4_BASE + SPI_SR_OFFSET))
+#define SPI4_DR                *((volatile uint32 *) (SPI4_BASE + SPI_DR_OFFSET))
+#define SPI4_CRCPR             *((volatile uint32 *) (SPI4_BASE + SPI_CRCPR_OFFSET))
+#define SPI4_RXCRCR            *((volatile uint32 *) (SPI4_BASE + SPI_RXCRCR_OFFSET))
+#define SPI4_TXCRCR            *((volatile uint32 *) (SPI4_BASE + SPI_TXCRCR_OFFSET))
+#define SPI4_I2SCFGR           *((volatile uint32 *) (SPI4_BASE + SPI_I2SCFGR_OFFSET))
+#define SPI4_I2SPR             *((volatile uint32 *) (SPI4_BASE + SPI_I2SPR_OFFSET))
+
+
 /*********************************************/
 /*              SPI_CR1 Bits                 */
 /*********************************************/
@@ -82,4 +125,66 @@
 #define SPI_CR1_BIDIOE      14
 #define SPI_CR1_BIDIMODE    15
 
+/*********************************************/
+/*              SPI_CR2 Bits                 */
+/*********************************************/
+#define SPI_CR2_RXDMAEN     0
+#define SPI_CR2_TXDMAEN     1
+#define SPI_CR2_SSOE        2
+
+#define SPI_CR2_FRF         4
+#define SPI_CR2_ERRIE       5
+#define SPI_CR2_RXNEIE      6
+#define SPI_CR2_TXEIE       7
+
+
+/*********************************************/
+/*              SPI_SR Bits                 */
+/*********************************************/
+#define SPI_SR_RXNE         0
+#define SPI_SR_TXE          1
+#define SPI_SR_CHSIDE       2
+#define SPI_SR_UDR          3
+#define SPI_SR_CRCERR       4
+#define SPI_SR_MODF         5
+#define SPI_SR_OVR          6
+#define SPI_SR_BSY          7
+#define SPI_SR_FRE          8
+
+
+/*********************************************/
+/*              SPI_I2SCFGR Bits                 */
+/*********************************************/
+#define SPI_I2SCFGR_CHLEN           0
+#define SPI_I2SCFGR_DATLEN          1
+#define SPI_I2SCFGR_DATLEN          2
+#define SPI_I2SCFGR_CKPOL           3
+#define SPI_I2SCFGR_I2SSTD          4
+#define SPI_I2SCFGR_I2SSTD          5
+
+#define SPI_I2SCFGR_PCMSYNC          7
+#define SPI_I2SCFGR_I2SCFG           8
+#define SPI_I2SCFGR_I2SCFG           9
+#define SPI_I2SCFGR_I2SE            10
+#define SPI_I2SCFGR_I2SMOD          11
+#define SPI_I2SCFGR_ASTREN          12
+
+
+
+
+
+/*********************************************/
+/*              SPI_I2SPR Bits                 */
+/*********************************************/
+#define SPI_I2SPR_I2SDIV           0
+#define SPI_I2SPR_I2SDIV           1
+#define SPI_I2SPR_I2SDIV           2
+#define SPI_I2SPR_I2SDIV           3
+#define SPI_I2SPR_I2SDIV           4
+#define SPI_I2SPR_I2SDIV           5
+#define SPI_I2SPR_I2SDIV           6
+#define SPI_I2SPR_I2SDIV           7
+
+#define SPI_I2SPR_ODD           8
+#define SPI_I2SPR_MCKOE           9
 
