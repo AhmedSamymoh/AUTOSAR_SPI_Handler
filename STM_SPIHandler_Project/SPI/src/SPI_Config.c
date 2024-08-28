681171336ee649d11ed0e5fca395c7a7a7863984
Rana Mohamed <144630250+RanaMohamed227@users.noreply.github.com>
2024-08-28 04:43:57 -0700

    [SPIH-41] Configure slave select pin ADDED THE PORTS FROM d-g

--- STM_SPIHandler_Project/SPI/src/SPI_Config.c
+++ STM_SPIHandler_Project/SPI/src/SPI_Config.c
@@ -35,6 +35,40 @@ switch(port)
 			// Set PIN to  medium speed mode
 	GPIOC->OSPEEDR |= (1<< (pin*2)U);
 			break;
+
+			
+			case PORTD:
+			// Set PIN to  General purpose output mode
+	GPIOD->MODER &= ~( (0x3UL << ((pin*2)U)) | ( 0x3UL << (((pin*2)+1)U) ) );
+	GPIOD->MODER |=  (0x3UL << ((pin*2)U));
+			
+			// Set PIN to  medium speed mode
+	GPIOD->OSPEEDR |= (1<< (pin*2)U);
+			break;
+			case PORTE:
+			// Set PIN to  General purpose output mode
+	GPIOE->MODER &= ~( (0x3UL << ((pin*2)U)) | ( 0x3UL << (((pin*2)+1)U) ) );
+	GPIOE->MODER |=  (0x3UL << ((pin*2)U));
+			
+			// Set PIN to  medium speed mode
+	GPIOE->OSPEEDR |= (1<< (pin*2)U);
+			break;
+			case PORTF:
+			// Set PIN to  General purpose output mode
+	GPIOF->MODER &= ~( (0x3UL << ((pin*2)U)) | ( 0x3UL << (((pin*2)+1)U) ) );
+	GPIOF->MODER |=  (0x3UL << ((pin*2)U));
+			
+			// Set PIN to  medium speed mode
+	GPIOF->OSPEEDR |= (1<< (pin*2)U);
+			break;
+				case PORTG:
+			// Set PIN to  General purpose output mode
+	GPIOG->MODER &= ~( (0x3UL << ((pin*2)U)) | ( 0x3UL << (((pin*2)+1)U) ) );
+	GPIOG->MODER |=  (0x3UL << ((pin*2)U));
+			
+			// Set PIN to  medium speed mode
+	GPIOG->OSPEEDR |= (1<< (pin*2)U);
+			break;
     }
 
 
