8f07fa8473e78eee1b1aeb9cb025ba11415bea9e
AhmedSamymoh <ahmeds.elnozahy@gmail.com>
2024-09-16 19:16:19 +0300

    [SPIH-122] Updating CI/CD Tools

--- STM_SPIHandler_Project/Src/main.c
+++ STM_SPIHandler_Project/Src/main.c
@@ -20,11 +20,52 @@
 #include "../COM/MCAL/SPI/inc/Spi.h"
 
 
+
+
+void SPI_TransmitReceive(uint8 *txData, uint8 *rxData, int size)
+{
+    int i = 0;
+
+    while (i < size)
+    {
+        // 1. Wait for the TXE bit to set in the Status Register (indicates buffer is empty)
+        while (!(SPI1->SR & (1 << 1))) {}
+
+        // 2. Write the data to the Data Register
+        SPI1->DR = txData[i];
+
+        // 3. Wait for the RXNE bit to set in the Status Register (indicates data is received)
+        while (!(SPI1->SR & (1 << 0))) {}
+
+        // 4. Read the received data from the Data Register
+        rxData[i] = SPI1->DR;
+
+        i++;
+    }
+
+    // 5. After the data has been transmitted, wait for the BSY bit to reset in Status Register
+    while (!(SPI1->SR & (1 << 1))) {} // Wait for TXE bit to set
+    while ((SPI1->SR & (1 << 7))) {}  // Wait for BSY bit to reset
+
+    // 6. Clear the Overrun flag by reading DR and SR
+    uint8_t temp = SPI1->DR;
+    temp = SPI1->SR;
+}
+
+uint8 rxData = 0;
+uint8 txData = 0xAA;
+uint8 * Data_tx = &txData;
+uint8 * Data_rx = &rxData;
 extern Spi_ConfigType Spi_Config;
+
 int main(void)
 {
     //System_Init();
 	Spi_Init(&Spi_Config);
+
+	//Spi_WriteIB(SPI_Channel_0, (uint16)Data_rx);
     /* Loop forever */
-	for(;;);
+	while(1){
+		SPI_TransmitReceive(Data_tx, Data_rx, 20);
+	}
 }
